8cf30d1a67039d7eeb691990efd2845c2fee5db3
Andy Knight <andym.knight@gmail.com>
2019-02-21 20:52:02 +0000

    initial commit

---
+++ app.js
@@ -0,0 +1,19 @@
+process.env['DEBUG'] = 'app';
+
+var express = require('express');
+var chalk = require('chalk');
+var debug = require('debug')('app');
+var morgan = require('morgan');
+var path = require('path');
+var app = express();
+
+app.use(morgan('tiny'));
+
+app.get('/',
+    (req, res) => {
+        res.sendFile(path.join(__dirname, 'views', 'index.html'));
+    });
+
+app.listen(3000, () => {
+    debug(`listening on port ${chalk.green('3000')}`);
+});
